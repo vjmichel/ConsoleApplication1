82f27e45079c682ce9b146c8d60e285efde6f7e0
vjmichel <vj.michel@yahoo.com>
2016-02-26 12:02:12 +0100

    console test app
    console test app

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+int main()
+{
+	printf("Hello world\n");
+    return 0;
+}
+
